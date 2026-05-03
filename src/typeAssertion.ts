07a208904024de74db10e279df3a231f7f909d6c
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-03 10:41:13 +0600

    practice type alias and interface

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -1,15 +1,18 @@
 let anything: any;
 anything = "suzan";
 
-const kgToGMConverter = (input: number | string): number | string | undefined => {
+//! Type Assertion তুমি TypeScript-কে বলতেছো — “এই ভ্যালুর টাইপ আমি তোমার চেয়ে ভালো জানি”
+
+const kgToGMConverter = (
+  input: number | string,
+): number | string | undefined => {
   if (typeof input === "number") {
     return input * 1000;
   } else if (typeof input === "string") {
     const [value] = input.split(" ");
     return Number(value) * 1000;
-}
+  }
 };
 
 const result1 = kgToGMConverter(10) as number;
 const result2 = kgToGMConverter("10") as number;
-
---
+++ src/typeInterface.ts
@@ -0,0 +1,32 @@
+//! type alias
+type User = {
+  name: string;
+  age: number;
+};
+
+//! interface use only : Object type like array object and function
+
+interface IUser {
+  name: string;
+  age: number;
+}
+
+type Role = {
+  role: "admin" | "user";
+};
+
+type UserWithRole = User & Role;
+
+const user1: UserWithRole = {
+  name: "suzan",
+  age: 20,
+  role: "admin",
+};
+
+const user2: IUser = {
+  name: "sheikh",
+  age: 30,
+};
+
+type IsAdmin = boolean;
+const isAdmin: IsAdmin = true;
