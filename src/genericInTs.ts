7932be649545e5bdd9f8bf87641a3593068d15fe
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-03 11:10:43 +0600

    use normal type array and use <> angle bracket type array

---
+++ src/genericInTs.ts
@@ -0,0 +1,10 @@
+
+//? use normal type array
+// const friends: string[] = ["mr. x", "mr. x", "mr. y"];
+// const rollNumber: number[] = [10, 20, 30];
+// const isEligibleList: boolean[] = [true, false]; 
+
+//? use <> type array
+const friends: Array<string> = ["mr. x", "mr. x", "mr. y"];
+const rollNumber: Array<number> = [10, 20, 30];
+const isEligibleList: Array<boolean> = [true, false]; 
