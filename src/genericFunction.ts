bc04e96bdbaa9c27d9aedbca53539a68374fdc86
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-03 13:43:25 +0600

    function genneries clear

--- src/genericFunction.ts
+++ src/genericFunction.ts
@@ -1,10 +1,6 @@
-
-
-
 const creteArrayWithGeneric = <T>(value: T) => {
-    return value
-}
-
+  return value;
+};
 
 // const arrString = creteArrayWithGeneric('Apple');
 // const arrString = creteArrayWithGeneric(200);
@@ -13,43 +9,44 @@ const creteArrayWithGeneric = <T>(value: T) => {
 //     name: 'suzan'
 // });
 
-
-console.log(creteArrayWithGeneric('apple'));
+console.log(creteArrayWithGeneric("apple"));
 console.log(creteArrayWithGeneric(120));
-console.log(creteArrayWithGeneric({
-    name: 'suzan',
+console.log(
+  creteArrayWithGeneric({
+    name: "suzan",
     age: 3251,
-}));
+  }),
+);
 
 //? tuple with generic
 
-const result = <X, Y> (param1: X, param2: Y) => {
-    return [param1, param2]
-}
+// const result = <X, Y> (param1: X, param2: Y) => {
+//     return [param1, param2]
+// }
 
 // console.log(result('suzan', 1230));
 
 const student1 = {
-    name: "suzan",
-    hasPen: true,
-}
+  name: "suzan",
+  hasPen: true,
+};
 const student2 = {
-    id: 12542,
-    name: 'jack',
-    hasCar: true,
-    isAuthor: true
-}
+  id: 12542,
+  name: "jack",
+  hasCar: true,
+  isAuthor: true,
+};
 
+type ExtData = {id: number; isAuthor: boolean}
 
+const addStudentToCourse = <T extends ExtData>(studentInfo: T) => {
+  return {
+    course: "Ai Dev",
+    ...studentInfo,
+  };
+};
 
-const addStudentToCourse = <T>(studentInfo: T) => {
-    return {
-        course: 'Ai Dev',
-        ...studentInfo
-    }
-}
+console.log(addStudentToCourse(student2));
 
-console.log(addStudentToCourse(student1);
-
-
-
+// const result = addStudentToCourse(student1);
+// console.log(result);
