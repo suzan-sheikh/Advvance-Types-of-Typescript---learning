18246f13f9d3a2a43cd0d82ad330a72647b5f141
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-03 11:36:26 +0600

    generich arry use with single argument and duble argument

--- src/genericInTs.ts
+++ src/genericInTs.ts
@@ -1,10 +1,22 @@
-
 //? use normal type array
 // const friends: string[] = ["mr. x", "mr. x", "mr. y"];
 // const rollNumber: number[] = [10, 20, 30];
-// const isEligibleList: boolean[] = [true, false]; 
+// const isEligibleList: boolean[] = [true, false];
 
 //? use <> type array
-const friends: Array<string> = ["mr. x", "mr. x", "mr. y"];
-const rollNumber: Array<number> = [10, 20, 30];
-const isEligibleList: Array<boolean> = [true, false]; 
+// const friends: Array<string> = ["mr. x", "mr. x", "mr. y"];
+// const rollNumber: Array<number> = [10, 20, 30];
+// const isEligibleList: Array<boolean> = [true, false];
+
+//? dynamically generalize Array
+
+type GeneralizeArray<T> = Array<T>
+
+const friends: GeneralizeArray<string> = ["mr. x", "mr. x", "mr. y"];
+const rollNumber: GeneralizeArray<number> = [10, 20, 30];
+const isEligibleList: GeneralizeArray<boolean> = [true, false];
+
+//? dynamically generalize Array with tow argument
+type Coordinates<X, Y> = [X, Y];
+const coordinates: Coordinates<string, string> = ["20", "30"]
+const coordinates2: Coordinates<string, number> = ["20", 30]
